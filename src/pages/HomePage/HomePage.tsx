4624cf784d69f4612ca9336edea5e8a6c0e470df
Michael Phelps <mpphelps@gmail.com>
2025-12-13 15:07:49 -0500

    Updated home page

--- src/pages/HomePage/HomePage.tsx
+++ src/pages/HomePage/HomePage.tsx
@@ -1,5 +1,16 @@
+import { Flex } from '@mantine/core';
+import ContactPage from '../ContactPage/ContactPage';
 import classes from './HomePage.module.css';
 
 export default function HomePage() {
-  return <div className={classes.homePage}>Welcome to my Website!</div>;
+  return (
+    <div className={classes.homePage}>
+      <p>Hi! I'm Michael Phelps, a passionate software engineer with focus on software and technology for industrial and engineering applications. Welcome to my Website!</p>
+      <p>Feel free to explore my projects and blogs. Contact me if you have any questions or opportunities!</p>
+
+      <Flex justify="center">
+        <ContactPage />
+      </Flex>
+    </div>
+  );
 }
